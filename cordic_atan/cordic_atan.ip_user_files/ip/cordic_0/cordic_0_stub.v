d1c0ebb7daf5676feaa8d54bff7036d4fb1d3125
Angelo Nutu <nutuangelo@yahoo.it>
2023-10-24 16:03:52 +0200

    riconfigurato il CORDIC

---
+++ cordic_atan/cordic_atan.ip_user_files/ip/cordic_0/cordic_0_stub.v
@@ -0,0 +1,25 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
+// Date        : Tue Oct 24 13:53:11 2023
+// Host        : thisguy running 64-bit Ubuntu 20.04.6 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/angelonutu/Desktop/Progetto-FInale-ADA/cordic_atan/cordic_atan.gen/sources_1/ip/cordic_0/cordic_0_stub.v
+// Design      : cordic_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "cordic_v6_0_18,Vivado 2022.2" *)
+module cordic_0(aclk, s_axis_cartesian_tvalid, 
+  s_axis_cartesian_tdata, m_axis_dout_tvalid, m_axis_dout_tdata)
+/* synthesis syn_black_box black_box_pad_pin="aclk,s_axis_cartesian_tvalid,s_axis_cartesian_tdata[15:0],m_axis_dout_tvalid,m_axis_dout_tdata[7:0]" */;
+  input aclk;
+  input s_axis_cartesian_tvalid;
+  input [15:0]s_axis_cartesian_tdata;
+  output m_axis_dout_tvalid;
+  output [7:0]m_axis_dout_tdata;
+endmodule
---
+++ cordic_atan/cordic_atan.ip_user_files/ip/cordic_0/cordic_0_stub.vhdl
@@ -0,0 +1,34 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
+-- Date        : Tue Oct 24 13:53:11 2023
+-- Host        : thisguy running 64-bit Ubuntu 20.04.6 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/angelonutu/Desktop/Progetto-FInale-ADA/cordic_atan/cordic_atan.gen/sources_1/ip/cordic_0/cordic_0_stub.vhdl
+-- Design      : cordic_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity cordic_0 is
+  Port ( 
+    aclk : in STD_LOGIC;
+    s_axis_cartesian_tvalid : in STD_LOGIC;
+    s_axis_cartesian_tdata : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    m_axis_dout_tvalid : out STD_LOGIC;
+    m_axis_dout_tdata : out STD_LOGIC_VECTOR ( 7 downto 0 )
+  );
+
+end cordic_0;
+
+architecture stub of cordic_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,s_axis_cartesian_tvalid,s_axis_cartesian_tdata[15:0],m_axis_dout_tvalid,m_axis_dout_tdata[7:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "cordic_v6_0_18,Vivado 2022.2";
+begin
+end;

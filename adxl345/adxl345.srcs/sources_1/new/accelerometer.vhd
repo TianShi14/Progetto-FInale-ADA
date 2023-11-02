ef35cbc3b0757f6ad5dc307e062fa69f38114bc2
Angelo Nutu <nutuangelo@yahoo.it>
2023-11-02 15:24:07 +0100

    segnale di enable per cordic msf

--- adxl345/adxl345.srcs/sources_1/new/accelerometer.vhd
+++ adxl345/adxl345.srcs/sources_1/new/accelerometer.vhd
@@ -5,7 +5,7 @@ entity accelerometer is
     generic(
         clk_freq       : integer := 100;                         -- frequenza del clock di sistema
         data_rate      : std_logic_vector := "1001";             -- rate con cui impostare l'acquisizione del dato dall'accelerometro ('1001' = 50 Hz -  ADXL345 Datasheet Reference) -> TODO?: implementare low-power setting?
-        data_range     : std_logic_vector := "00"                -- range di misurazione del dato ('00' = ± 2g - ADXL345 Datasheet Reference)
+        data_range     : std_logic_vector := "00"                -- range di misurazione del dato ('00' = ï¿½ 2g - ADXL345 Datasheet Reference)
     );
     port(
         clk            : in      std_logic;                      -- clock di sistema
@@ -14,7 +14,9 @@ entity accelerometer is
         sclk           : buffer  std_logic;                      -- clock SPI (SPI reference)
         cs             : buffer  std_logic;                      -- chip select (SPI reference)
         mosi           : out     std_logic;                      -- Master Our Slave In (SPI Reference)
-        acceleration_x : out     std_logic_vector(15 downto 0)   -- accelerazione asse x
+        cordic_ena     : out     std_logic;                      -- abilita il calcolo dell'atan2
+        acceleration_x : out     std_logic_vector(15 downto 0);  -- accelerazione asse x
+        acceleration_y : out     std_logic_vector(15 downto 0)   -- accelerazione asse y
     );    
 end accelerometer;
 
@@ -24,7 +26,7 @@ architecture behavioral of accelerometer is
     signal prev_busy          : std_logic;                              -- componente precedentemente occupata
     signal curr_busy          : std_logic;                              -- componente attualmente occupata
     signal spi_ena            : std_logic;                              -- abilita la comunicazione
-    signal spi_cont           : std_logic;                              -- flag per modalità continua (SPI reference)
+    signal spi_cont           : std_logic;                              -- flag per modalitï¿½ continua (SPI reference)
     signal parameter          : integer range 0 to 3;                   -- parametro da configurare (rate, range, acquire, etc. - ADXL345 Datasheet Reference)
     signal parameter_data     : std_logic_vector(3 downto 0);           -- valore da sovrascrivere nel registro del parametro 
     signal parameter_addr     : std_logic_vector(5 downto 0);           -- indirizzo del registro col parametro da configurare   
@@ -33,7 +35,6 @@ architecture behavioral of accelerometer is
     signal acc_x_buff         : std_logic_vector(15 downto 0);          -- buffer accelerazione sull'asse x
     signal acc_y_buff         : std_logic_vector(15 downto 0);          -- buffer accelerazione sull'asse y
     signal acc_z_buff         : std_logic_vector(15 downto 0);          -- buffer accelerazione sull'asse z
-    signal acceleration_y     : std_logic_vector(15 downto 0);          -- PLACEHOLDER 
     signal acceleration_z     : std_logic_vector(15 downto 0);          -- PLACEHOLDER 
     
     component spi_master is
@@ -43,17 +44,17 @@ architecture behavioral of accelerometer is
         port(
             clk      : in     std_logic;                                -- clock di sistema                       
             reset    : in     std_logic;                                -- reset asincrono attivo basso
-            cpol     : in     std_logic;                                -- polarità (SPI reference)
+            cpol     : in     std_logic;                                -- polaritï¿½ (SPI reference)
             cpha     : in     std_logic;                                -- fase (SPI reference)
             miso     : in     std_logic;                                -- Master In Slave Out (SPI reference)
             enable   : in     std_logic;                                -- segnale di avvio transazione
             cont     : in     std_logic;                                -- segnale di continua transazione
             divider  : in     integer;                                  -- divisore del clock f(sclk) = f(clk)/(2 * divider)
-            transmit : in     std_logic_vector(width - 1 downto 0);     -- dati da trasmettere (indirizzi registri, sensibilità acquisizione dati, etc.)                     
+            transmit : in     std_logic_vector(width - 1 downto 0);     -- dati da trasmettere (indirizzi registri, sensibilitï¿½ acquisizione dati, etc.)                     
             sclk     : buffer std_logic;                                -- clock SPI (SPI reference)
             cs       : buffer std_logic;                                -- chip select (SPI reference)
             mosi     : out    std_logic;                                -- Master Our Slave In (SPI Reference)
-            busy     : out    std_logic;                                -- segnala se la transazione è in corso
+            busy     : out    std_logic;                                -- segnala se la transazione ï¿½ in corso
             receive  : out    std_logic_vector(width - 1 downto 0)      -- dato ricevuto dallo slave
         ); 
     end component spi_master;
@@ -91,7 +92,8 @@ begin
             transmit       <= (others => '0');    
             acceleration_x <= (others => '0'); 
             acceleration_y <= (others => '0'); 
-            acceleration_z <= (others => '0'); 
+            acceleration_z <= (others => '0');
+            cordic_ena     <= '0'; 
             state          <= start;                    
         elsif rising_edge(clk) then  
             case state is                       
@@ -101,7 +103,8 @@ begin
                     parameter <= 0;  
                     state     <= hold;
           
-                when hold =>                                                -- attesa di 200ns tra ogni transazione (ADXL345 Datasheet Reference)                     
+                when hold =>                                                -- attesa di 200ns tra ogni transazione (ADXL345 Datasheet Reference)
+                    cordic_ena <= '0';                     
                     if curr_busy = '0' then                                 
                         if counter < clk_freq/5 then                        -- non sono passati 200ns
                             counter := counter + 1;                         -- aumenta il contatore
@@ -134,7 +137,7 @@ begin
                 when setup =>
                     prev_busy <= curr_busy;                
                     
-                    if prev_busy = '1' and curr_busy = '0' then             -- busy appena tirato giù
+                    if prev_busy = '1' and curr_busy = '0' then             -- busy appena tirato giï¿½
                         counter := counter + 1;                             -- conta il numero di volte che la comunicazione si libera
                     end if;
                     
@@ -144,10 +147,10 @@ begin
                                 spi_cont    <= '1';                         -- teniamo conto del dato da inviare per mantenere la comunicazione attiva
                                 spi_ena     <= '1';                         -- diamo inizio alla comunicazione seriale
                                 transmit    <= "00" & parameter_addr;       -- diciamo allo slave che registro vogliamo modificare
-                            else                                            -- la prima transazione è iniziata
-                                transmit    <= "0000" & parameter_data;     -- passiamo subito il valore da caricare nel registro (il buffer interno SPI ha già memorizzato) 
+                            else                                            -- la prima transazione ï¿½ iniziata
+                                transmit    <= "0000" & parameter_data;     -- passiamo subito il valore da caricare nel registro (il buffer interno SPI ha giï¿½ memorizzato) 
                             end if;
-                        when 1 =>                                           -- linea si è liberata, tagliamo la comunicazione
+                        when 1 =>                                           -- linea si ï¿½ liberata, tagliamo la comunicazione
                             spi_cont <= '0';                               
                             spi_ena  <= '0';                                
                             counter  := 0;                                  
@@ -158,7 +161,7 @@ begin
                 when read =>                                            -- lettura dei dati dell'accelerometro
                     prev_busy <= curr_busy;                        
                     
-                    if prev_busy = '1' and curr_busy = '0' then         -- busy appena tirato giù                                 
+                    if prev_busy = '1' and curr_busy = '0' then         -- busy appena tirato giï¿½                                 
                         counter := counter + 1;                         -- conta il numero di volte che la comunicazione si libera
                     end if;
                               
@@ -168,7 +171,7 @@ begin
                                 spi_cont    <= '1';                     -- teniamo conto del dato da inviare per mantenere la comunicazione attiva                        
                                 spi_ena     <= '1';                     -- diamo inizio alla comunicazione seriale                                                        
                                 transmit    <= "11110010";              -- diciamo allo slave che comando vogliamo eseguire (ADXL345 Datasheet Reference)                                         
-                            else                                        -- la prima transazione è iniziata                                                                
+                            else                                        -- la prima transazione ï¿½ iniziata                                                                
                                 transmit    <= "00000000";              -- valore nullo per la conferma della tipologia di comunicazione  
                             end if;
                         when 2 =>                                       -- seconda volta che si libera la linea 
@@ -180,7 +183,7 @@ begin
                         when 5 =>                                       -- quinta volta che si libera la linea           
                             acc_y_buff(15 downto 8) <= receive;         -- carichiamo nel buffer gli ultimi 8 bit dell'asse y
                         when 6 =>                                       -- sesta volta che si libera la linea
-                            spi_cont                <= '0';             -- togliamo in anticipo il flag per la modalità continua, per interrompere il burst
+                            spi_cont                <= '0';             -- togliamo in anticipo il flag per la modalitï¿½ continua, per interrompere il burst
                             spi_ena                 <= '0';             -- termina la comunicazione in modo che non si prosegua con un altra transazione
                             acc_z_buff(7 downto 0)  <= receive;         -- carichiamo nel buffer i primi 8 bit dell'asse z
                         when 7 =>                                       -- settima volta che si libera la linea 
@@ -191,6 +194,7 @@ begin
                     end case;
   
                 when output =>
+                    cordic_ena     <= '1';         -- segnala che le uscite si sono aggiornate e il CORDIC puÃ² calcolare l'atan2
                     acceleration_x <= acc_x_buff;  -- output asse x
                     acceleration_y <= acc_y_buff;  -- output asse y
                     acceleration_z <= acc_z_buff;  -- output asse z
